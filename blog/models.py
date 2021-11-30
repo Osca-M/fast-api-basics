5089c9846911c13a7ba9ccbb57a15b71f987ac19
Osca Mwongera <oscamwongera@gmail.com>
2021-11-30 15:45:30 +0300

    User model

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,11 @@ class Blog(Base):
     title = Column(String,)
     body = Column(String,)
     published = Column(Boolean,)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = Column(String,)
+    email = Column(String,)
+    password = Column(String,)
